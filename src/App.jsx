b9feeeb754cd69478bb2f73cae7a97e19eb013cd
NahuelAbadia <abadianahuel@hotmail.com.ar>
2022-04-21 18:39:28 -0300

    food app

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,18 @@
 import React from 'react';
-import Login from './auth/Login'
 import Home from './components/views/Home';
 import Landing from './components/Landing';
-import PrivateRoutes from './auth/PrivateRoutes';
 import Error404 from './auth/Error404';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { useSelector } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
 function App() {
-
-  const Token = useSelector(state => state.token)
 
   return (
     <>
       <Router>
         <Routes>
-          <Route exact path='/' element={<Login />} />
-          <Route element={<PrivateRoutes Token={Token} />}>
-            <Route exact path='/search' element={<Landing />} />
+            <Route exact path='/' element={<Landing />} />
             <Route exact path='/home' element={<Home />} />
-          </Route>
           <Route path='*' element={<Error404 />} />
         </Routes>
       </Router>
